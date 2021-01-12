30f3c0e22bdbad735a75df2f24ee2e7e80772de8
novalkris18 <72119213+novalkris18@users.noreply.github.com>
2021-01-12 23:54:37 +0700

    Add files via upload

---
+++ web kelompok final/file-js/script.js
@@ -0,0 +1,8 @@
+var ok = confirm("Silahkan Login jika sudah memiliki akun")
+
+if (ok) {
+    document.write = "masuk.html"
+}
+else {
+    window.location = "index.html"
+}
---
+++ web kelompok final/file-js/script2.js
@@ -0,0 +1,8 @@
+var ok = confirm("Apakah anda ingin mendaftar ?")
+
+if (ok) {
+    document.write = "daftar.html"
+} 
+else {
+    window.location = "index.html"
+}
